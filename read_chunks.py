ced6d3cbca40eaf65f1585d6c3482c34d1fcb53f
Navneet Singh <38272714+navneet-singh2907@users.noreply.github.com>
2026-05-29 14:36:16 -0400

    Feat: Integrate Pandas DataFrame factory with local Ollama batch embedding pipeline

--- read_chunks.py
+++ read_chunks.py
@@ -1,14 +1,39 @@
 import requests
 import json
 import os
+import pandas as pd 
 
 
-def create_embedding(text):
+def create_embedding(text_list):
 
-    r =requests.post("http://localhost:11434/api/embeddings", json={"model": "bge-m3", "prompt": text })
-    embedding = r.json()["embedding"]
+    r =requests.post("http://localhost:11434/api/embed", json={"model": "bge-m3", "input": text_list })
+    embedding = r.json()["embeddings"]
 
     return embedding
 
-a = create_embedding("This is a sample text to create an embedding.")
-print(a)
+# a = create_embedding(["hello world", "how are you?"])
+# print(a)  
+ 
+
+transcripsts = os.listdir('transcripts')
+my_data = []
+chunk_id = 0
+for transcript in transcripsts:
+    if not transcript.endswith('.json'):
+        continue
+
+    with open(f'transcripts/{transcript}', 'r') as f:
+        data = json.load(f)
+    print(f"Processing {transcript} with {len(data)} chunks.")
+    embeddings = create_embedding([chunk["text"] for chunk in data])
+    for i, chunk in enumerate(data):
+        chunk["id"] = chunk_id
+        chunk_id += 1
+        chunk["embedding"] = embeddings[i]
+        my_data.append(chunk)
+        
+df = pd.DataFrame.from_records(my_data)
+
+df.to_pickle("transcripts_with_vectors.pkl")
+print("\n[Success] Master DataFrame saved to transcripts_with_vectors.pkl")
+    
